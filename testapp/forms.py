7efeb4cce0c09aff0d60dcaaf45362450c8fe26f
Dmitriy Fomin <159246105+dfomin1984@users.noreply.github.com>
2024-09-17 17:51:17 +0300

    Update forms.py

--- testapp/forms.py
+++ testapp/forms.py
@@ -4,4 +4,4 @@ from testapp.models import product
 class addPostFormProduct(forms.Form):
     pName = forms.CharField(max_length=250,min_length=1,label='Название')
     pDescription = forms.CharField(widget=forms.Textarea(),required=False,label='Описание')
-    pPrice = forms.DecimalField( max_digits=10, decimal_places=2,label='Цена')
+    pPrice = forms.DecimalField( max_digits=10, decimal_places=2,label='Цена',min_value=0.01)
